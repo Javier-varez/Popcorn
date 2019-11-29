9e8f694152b1ff782f924aff926c090cd57f9128
Javier Álvarez <javier.alvarez@allthingsembedded.net>
2019-11-29 23:22:19 +0100

    Add proper SVC Handler

--- Src/os.c
+++ Src/os.c
@@ -1,6 +1,15 @@
 #include "os.h"
 #include "stm32f1xx.h"
 
+#define __NAKED				__attribute__((naked))
+
+#define STRINGIZE(_x)		#_x
+#define SVC_CALL(_x)		asm volatile("svc " STRINGIZE(_x))
+
+#define START_OS_SVC		2
+
+#define EXC_RETURN_PSP_UNPRIV		0xFFFFFFFD
+
 volatile int scheduler_active = 0;
 
 static volatile void* tasks_psp[MAX_TASKS];
@@ -17,9 +26,9 @@ struct task_stack_frame
 	uint32_t r3;
 	uint32_t r12;
 	uint32_t lr;
-	uint32_t ret_addr;
+	uint32_t pc;
 	uint32_t xpsr;
-} __attribute__((packed));
+} __PACKED;
 
 struct complete_task_stack_frame
 {
@@ -33,7 +42,7 @@ struct complete_task_stack_frame
        uint32_t r11;
        uint32_t lr;
        struct task_stack_frame autosave;
-} __attribute__((packed));
+} __PACKED;
 
 void CreateTask(task_func func)
 {
@@ -42,7 +51,7 @@ void CreateTask(task_func func)
 		n_tasks++;
 		struct complete_task_stack_frame* task_psp = 
 			(struct complete_task_stack_frame*) &stack[STACK_SIZE - 1 - TASK_STACK_SIZE * n_tasks - sizeof(struct complete_task_stack_frame)];
-		task_psp->autosave.ret_addr = (uint32_t)func;
+		task_psp->autosave.pc = (uint32_t)func;
 		task_psp->autosave.xpsr = 1 << 24;
 		task_psp->autosave.lr = 0xA5A5A5A5;
         task_psp->lr = 0xfffffffd;
@@ -50,11 +59,11 @@ void CreateTask(task_func func)
 	}
 }
 
-void SVC_Handler()
+static uint32_t StartOS_SVC_Handler()
 {
 	scheduler_active = 1;
-    __NVIC_SetPriority(SysTick_IRQn, 0);
-    __NVIC_SetPriority(PendSV_IRQn, 3);
+    __NVIC_SetPriority(SysTick_IRQn, 0x00);
+    __NVIC_SetPriority(PendSV_IRQn,  0xFF);
 
     struct complete_task_stack_frame* frame =
         (struct complete_task_stack_frame*)tasks_psp[0];
@@ -62,16 +71,13 @@ void SVC_Handler()
 	// Start executing first task
 	__set_PSP((uint32_t)&frame->autosave);
 	__set_CONTROL(0x03);
-    asm volatile (
-        "ldr lr, immediate_val\n"
-        "bx lr\n"
-        "immediate_val: .word 0xfffffffd"
-    );
+
+	return EXC_RETURN_PSP_UNPRIV;
 }
 
-void PendSV_Handler()
+__NAKED void PendSV_Handler()
 {
-	asm volatile(
+	asm volatile (
 		"    mrs r0, psp                   \n"
 		"    isb                           \n"
 		"    ldr r1, pxCurrentTask         \n"
@@ -87,10 +93,48 @@ void PendSV_Handler()
 		"    isb                           \n"
 		"    bx lr                         \n"
 		"pxCurrentTask: .word current_task \n"
-		"pxTasksPSP: .word tasks_psp");
+		"pxTasksPSP: .word tasks_psp         "
+	);
+}
+
+__NAKED void SVC_Handler()
+{
+	asm volatile (
+		"    tst lr, #4              \n"
+		"    ite eq                  \n"
+		"    mrseq r0, msp           \n"
+		"    mrsne r0, psp           \n"
+		"    b SVC_Handler_C           "
+	);
+}
+
+void SVC_Handler_C(struct task_stack_frame* args)
+{
+	char* pc = (char*)args->pc;
+	uint8_t svc_code = pc[-2]; // First byte of svc instruction
+	uint32_t lr = 0;
+
+	switch (svc_code)
+	{
+	case START_OS_SVC:
+		lr = StartOS_SVC_Handler();
+		break;
+	
+	default:
+		while (1);
+		break;
+	}
+
+	// Update lr if required
+	asm volatile(
+		"    teq %[lr], #0			\n"
+		"    it ne								\n"
+		"    movne lr, %[lr]			  "
+		: : [lr] "r" (lr) 
+	);
 }
 
 void StartOS()
 {
-    asm volatile("svc 0");
+	SVC_CALL(START_OS_SVC);
 }
