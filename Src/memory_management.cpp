d40282547bcbcb04836ace9a595c27812c8be6e9
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2020-01-25 11:13:54 +0100

    Reentrant memory allocation model.
    Ref #1

--- Src/memory_management.cpp
+++ Src/memory_management.cpp
@@ -1,16 +1,25 @@
-#include "memory_management.h"
-#include "platform.h"
 #include <stdlib.h>
 
+#include "memory_management.h"
+#include "platform.h"
+#include "spinlock.h"
 
+static OS::SpinLock lock;
 
 CLINKAGE void* OsMalloc(size_t size)
 {
+    void *ptr = nullptr;
     // TODO: Migrate to custom allocation scheme
-    return malloc(size);
+    lock.Lock();
+    ptr = malloc(size);
+    lock.Unlock();
+
+    return ptr;
 }
 
 CLINKAGE void OsFree(void* ptr)
 {
+    lock.Lock();
     free(ptr);
+    lock.Unlock();
 }
